3134e3fce7d3b370ab910574d421b02e1454f04b
msycat <msycat@163.com>
2018-01-05 22:17:54 +0800

    1.5

--- src/router/index.js
+++ src/router/index.js
@@ -28,29 +28,6 @@ import tag1 from '@/components/collect/tag-1.vue'
 import tag2 from '@/components/collect/tag-2.vue'
 
 
-<<<<<<< HEAD
-const routes = [
-  {path:'',redirect:'/mains'},  //重定向，为空的时候进入到main文件里
-  {path:'/mains',name:'mains',component:index},
-  {path:'/tracks',name:'tracks',component:tracks},
-  {path:'/collect',name:'collect',component:collect,
-    children:[
-      {path:'/collect',redirect:'/tag1'},
-      {path:'/tag1',component:tag1,name:'tag1'},
-      {path:'/tag2',component:tag2,name:'tag2'}
-    ]},
-  {path:'/order',name:'order',component:order,
-    children:[
-      // {path:'',redirect:'or1',name:''},
-      {path:'/or1',component:or1,name:'or1'},
-      {path:'/or2',component:or2,name:'or2'},
-      {path:'/or3',component:or3,name:'or3'},
-      {path:'/or4',component:or4,name:'or4'},
-      {path:'/or5',component:or5,name:'or5'}
-    ]},
-  {path:'/mine',name:'mine',component:mine},
-  {path:'/wallet',component:wallet,name:'wallet'},
-=======
  const routes = [
     {path:'',redirect:'/mains'},  //重定向，为空的时候进入到main文件里
     {path:'/mains',name:'mains',component:index},
@@ -72,7 +49,6 @@ const routes = [
       ]},
     {path:'/mine',name:'mine',component:mine},
     {path:'/wallet',component:wallet,name:'wallet'},
->>>>>>> origin/GF
     {path:'/sale',component:sale,name:'sale'},
     {path:'/bankcard',component:bankcard,name:'bankcard'},
     {path:'/need',component:need,name:'need'},
@@ -80,13 +56,8 @@ const routes = [
     {path:'/release',component:release,name:'release'},
     {path:'/setup',component:setup,name:'setup'},
     {path:'/connect',component:connect,name:'connect'},
-<<<<<<< HEAD
-  {path:'**',redirect:'/mains'}
-]
-=======
     {path:'**',redirect:'/mains'}
  ]
->>>>>>> origin/GF
      
   
 
